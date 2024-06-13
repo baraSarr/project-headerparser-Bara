e35dc8c77bb3de84353eacb70bbc433974338cf5
Bara SaRR <bara.sarr@outlook.fr>
2024-06-13 10:40:53 +0000

    first try

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function(req,res) {
+  ipAddress = req.ip;
+  preferedLanguage = req.headers["accept-language"];
+  agent = req.headers["user-agent"];
+  res.json({ipaddress: ipAddress, language: preferedLanguage, software: agent});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
